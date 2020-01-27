472f976ed15b4ba1a8a9dbcaa4ec0cbc5049b357
M5MAT <m5mat@deadbeef.cafe>
2020-01-27 16:54:56 +0000

    Added re-connection

--- aprs_bridge/src/main/java/cafe/deadbeef/aprs_bridge/AprsClient.java
+++ aprs_bridge/src/main/java/cafe/deadbeef/aprs_bridge/AprsClient.java
@@ -58,79 +58,81 @@ public class AprsClient implements ApplicationListener<AprsPacketEvent> {
     private boolean connected = false;
 	private Parser aprsParser = new Parser();
 	
-	
-	
 	//@Async
 	@EventListener
 	public void onApplicationEvent(ContextRefreshedEvent event) {
 		logger.info("Starting AprsClient...");
 		
-		
-		try {
-			clientSocket = new Socket(server, port);
-			out = new PrintWriter(clientSocket.getOutputStream(), true);
-			BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-			
-			// Do login
-			Thread.sleep(500);
-			logger.info("Using login string: " + this.getLoginString());
-			out.println(this.getLoginString());
-			
-			// Construct a dummy packet
-			InformationField info = new MessagePacket("2E1HNK", "Testing", "001");
-			APRSPacket response = new APRSPacket("2E1HNK", "ECHO", null, info);
-			
-			logger.info("Sample Packet: " + response.toString());
-			
-			connected = true;
-			
-			while (true) {
-				String line = in.readLine();
-
-				// Skip any comment lines
-				if (!line.startsWith("#")) {
-					if (line != null) {
-						try {
-							APRSPacket packet = aprsParser.parse(line);
-							// messageHandler.handle(packet);
-							switch (packet.getDti()) {
-							case '!':
-							case '/':
-							case '@':
-							case '=':
-							case '\'':
-							case '`':
-							case '$':
-								// Position
-								logger.debug(String.format("(%s >%s> %s) %.3f, %.3f", packet.getSourceCall(), packet.getDti(), packet.getDestinationCall(), ((PositionPacket)packet.getAprsInformation()).getPosition().getLatitude(), ((PositionPacket)packet.getAprsInformation()).getPosition().getLongitude()));
-								applicationEventPublisher.publishEvent(new AprsPositionEvent(this, packet));
-								break;
-							case ':':
-								// Message
-								logger.debug(String.format("(%s >%s> %s) %s", packet.getSourceCall(), packet.getDti(), ((MessagePacket)packet.getAprsInformation()).getTargetCallsign(), ((MessagePacket)packet.getAprsInformation()).getMessageBody()));
-								applicationEventPublisher.publishEvent(new AprsMessageEvent(this, packet));
-								break;
-							
-							default:
-								// Unknown
-								logger.error(String.format("(%s >%s> %s) UNKNOWN", packet.getSourceCall(), packet.getDti(), packet.getDestinationCall()));
-								break;
+		while (true) {
+			try {
+				clientSocket = new Socket(server, port);
+				out = new PrintWriter(clientSocket.getOutputStream(), true);
+				BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+				
+				// Do login
+				Thread.sleep(500);
+				logger.info("Using login string: " + this.getLoginString());
+				out.println(this.getLoginString());
+				
+				// Construct a dummy packet
+				InformationField info = new MessagePacket("2E1HNK", "Testing", "001");
+				APRSPacket response = new APRSPacket("2E1HNK", "ECHO", null, info);
+				
+				logger.info("Sample Packet: " + response.toString());
+				
+				connected = true;
+				
+				while (true) {
+					String line = in.readLine();
+	
+					// Skip any comment lines
+					if (!line.startsWith("#")) {
+						if (line != null) {
+							try {
+								APRSPacket packet = aprsParser.parse(line);
+								// messageHandler.handle(packet);
+								switch (packet.getDti()) {
+								case '!':
+								case '/':
+								case '@':
+								case '=':
+								case '\'':
+								case '`':
+								case '$':
+									// Position
+									logger.debug(String.format("(%s >%s> %s) %.3f, %.3f", packet.getSourceCall(), packet.getDti(), packet.getDestinationCall(), ((PositionPacket)packet.getAprsInformation()).getPosition().getLatitude(), ((PositionPacket)packet.getAprsInformation()).getPosition().getLongitude()));
+									applicationEventPublisher.publishEvent(new AprsPositionEvent(this, packet));
+									break;
+								case ':':
+									// Message
+									logger.debug(String.format("(%s >%s> %s) %s", packet.getSourceCall(), packet.getDti(), ((MessagePacket)packet.getAprsInformation()).getTargetCallsign(), ((MessagePacket)packet.getAprsInformation()).getMessageBody()));
+									applicationEventPublisher.publishEvent(new AprsMessageEvent(this, packet));
+									break;
+								
+								default:
+									// Unknown
+									logger.error(String.format("(%s >%s> %s) UNKNOWN", packet.getSourceCall(), packet.getDti(), packet.getDestinationCall()));
+									break;
+								}
+								
+							} catch (Exception e) {
+								// logger.error("Oops, exception thrown!", e);
+								// e.printStackTrace();
 							}
-							
-						} catch (Exception e) {
-							// logger.error("Oops, exception thrown!", e);
-							// e.printStackTrace();
 						}
+						Thread.sleep(100);
 					}
-					Thread.sleep(100);
 				}
+			} catch (Exception e) {
+				e.printStackTrace();
+				try {
+					// This is probably a connection problem, wait 10 secs then try again
+					Thread.sleep(10000);
+				} catch (InterruptedException e1) {
+					e1.printStackTrace();
+				}
+				
 			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InterruptedException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
 		}
 	}
 	
